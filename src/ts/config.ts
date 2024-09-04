70f4e19892fda71150e9894839851de2b2078b37
hikaelis <sileakih@gmail.com>
2024-09-04 17:29:03 +0900

    fix: リレー変えた

--- src/ts/config.ts
+++ src/ts/config.ts
@@ -1,12 +1,14 @@
 // リレーURLの配列(投稿用)
 export const relayURLs = [
-  'wss://relay.damus.io',
-  'wss://ren.nostr1.com',
-  'wss://riray.nostr1.com',
-  'wss://r.kojira.io',
-  'wss://nostr-relay.nokotaro.com',
-  'wss://yabu.me'
+  "wss://nos.lol",
+  "wss://relay.damus.io",
+  "wss://relay-jp.shino3.net",
+  "wss://yabu.me",
+  "wss://relay.nostr.wirednet.jp",
+  "wss://nrelay-jp.c-stellar.net",
+  "wss://nostr-relay.nokotaro.com",
+  "wss://relay-jp.nostr.wirednet.jp",
+  "wss://r.kojira.io",
   ]
-
 
 
